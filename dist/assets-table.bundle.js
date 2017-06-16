04019080551ddbf89dfc64cc62fbf058055058ad
Andrey Bayandin <dontusecascade@gmail.com>
2017-06-16 15:18:21 +0500

    fix(front-end): Доработки
    https://jira.railsc.ru/browse/GOODS-589

--- dist/assets-table.bundle.js
+++ dist/assets-table.bundle.js
@@ -78,7 +78,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "20041f0dca40dc9e0d36"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "ca10d5535c5ed8020bfa"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -19026,23 +19026,23 @@ return /******/ (function(modules) { // webpackBootstrap
 	        null,
 	        state.rejectedFiles.map(function (file) {
 	          return (0, _attrAccept2.default)(file, props.accept) ? _react2.default.createElement(
-	            'p',
+	            'div',
 	            { className: b('error') },
 	            file.name,
 	            ' - \u041F\u0440\u0435\u0432\u044B\u0448\u0435\u043D \u0434\u043E\u043F\u0443\u0441\u0442\u0438\u043C\u044B\u0439 \u0440\u0430\u0437\u043C\u0435\u0440'
 	          ) : _react2.default.createElement(
-	            'p',
+	            'div',
 	            { className: b('error') },
 	            '\u041D\u0435\u0432\u043E\u0437\u043C\u043E\u0436\u043D\u043E \u043F\u0440\u0438\u043A\u0440\u0435\u043F\u0438\u0442\u044C \u0444\u0430\u0439\u043B. \u0421\u043E\u0434\u0435\u0440\u0436\u0430\u043D\u0438\u0435 \u0444\u0430\u0439\u043B\u0430, \u043D\u0435 \u043F\u043E\u0434\u0434\u0435\u0440\u0436\u0438\u0432\u0430\u0435\u0442 \u0444\u043E\u0440\u043C\u0430\u0442 \u0438\u0437\u043E\u0431\u0440\u0430\u0436\u0435\u043D\u0438\u044F.'
 	          );
 	        }),
 	        _this.getTotalCount() >= props.maxLenght && _react2.default.createElement(
-	          'p',
+	          'div',
 	          { className: b('error') },
 	          '\u0412\u044B \u0432\u044B\u0431\u0440\u0430\u043B\u0438 \u043C\u0430\u043A\u0441\u0438\u043C\u0443\u043C \u0444\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0438\u0439!'
 	        ),
 	        props.error && _react2.default.createElement(
-	          'p',
+	          'div',
 	          { className: b('error') },
 	          '\u041D\u0435 \u0443\u0434\u0430\u043B\u043E\u0441\u044C \u0437\u0430\u0433\u0440\u0443\u0437\u0438\u0442\u044C \u0444\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0438\u0438, \u043F\u043E\u0432\u0442\u043E\u0440\u0438\u0442\u0435 \u043F\u043E\u043F\u044B\u0442\u043A\u0443.'
 	        )
@@ -19108,12 +19108,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	            null,
 	            _react2.default.createElement(
 	              'h3',
-	              null,
+	              { className: b('title') },
 	              '\u0417\u0430\u0433\u0440\u0443\u0437\u043A\u0430 \u0444\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0438\u0439'
 	            ),
 	            count ? _react2.default.createElement(
-	              'p',
-	              null,
+	              'div',
+	              { className: b('choose') },
 	              '\u0412\u044B \u0432\u044B\u0431\u0440\u0430\u043B\u0438 ',
 	              count,
 	              ' ',
@@ -19122,7 +19122,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	              ' ',
 	              props.maxLenght
 	            ) : _react2.default.createElement(
-	              'p',
+	              'div',
 	              null,
 	              '\u0412\u044B\u0431\u0435\u0440\u0438\u0442\u0435 \u0444\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0438\u044E, \u043A\u043E\u0442\u043E\u0440\u0430\u044F \u043F\u043E\u0434\u0445\u043E\u0434\u0438\u0442 \u0434\u0430\u043D\u043D\u043E\u0439 \u0433\u0440\u0443\u043F\u043F\u0435 \u0442\u043E\u0432\u0430\u0440\u043E\u0432 \u0431\u043E\u043B\u044C\u0448\u0435 \u0432\u0441\u0435\u0433\u043E.'
 	            ),
@@ -19150,9 +19150,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	          _react2.default.createElement(
 	            'div',
 	            null,
-	            _react2.default.createElement(
-	              'p',
-	              null,
+	            !(count >= props.maxLenght) && _react2.default.createElement(
+	              'div',
+	              { className: b('upload-title') },
 	              '\u0417\u0430\u0433\u0440\u0443\u0437\u0438\u0442\u0435 \u043A\u0430\u0440\u0442\u0438\u043D\u043A\u0443'
 	            ),
 	            _react2.default.createElement(
